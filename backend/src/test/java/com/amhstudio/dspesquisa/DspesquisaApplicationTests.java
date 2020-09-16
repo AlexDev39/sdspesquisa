d4fe12c8aab1c1459858c9617faf2f3a943c1440
AlexDev39 <alexhavilla@gmail.com>
2020-09-16 09:28:10 -0300

    Project created

---
+++ backend/src/test/java/com/amhstudio/dspesquisa/DspesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amhstudio.dspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
