8d141502863c59e96dafe097a0eca4684ce85c5a
AlexDev39 <alexhavilla@gmail.com>
2020-09-17 23:31:04 -0300

    Post, Records

---
+++ backend/src/main/java/com/amhstudio/dspesquisa/services/RecordService.java
@@ -0,0 +1,40 @@
+package com.amhstudio.dspesquisa.services;
+
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.amhstudio.dspesquisa.dto.RecordDTO;
+import com.amhstudio.dspesquisa.dto.RecordInsertDTO;
+import com.amhstudio.dspesquisa.entities.Game;
+import com.amhstudio.dspesquisa.entities.Record;
+import com.amhstudio.dspesquisa.repository.GameRepository;
+import com.amhstudio.dspesquisa.repository.RecordRepository;
+
+@Service
+public class RecordService {
+	
+	@Autowired
+	private RecordRepository repository;
+	
+	@Autowired
+	private GameRepository gameRepository;
+	
+	@Transactional
+	public RecordDTO insert(RecordInsertDTO dto) {
+		Record entity = new Record();
+		entity.setName(dto.getName());
+		entity.setAge(dto.getAge());
+		entity.setMoment(Instant.now());
+		
+		Game game = gameRepository.getOne(dto.getGameId());
+		entity.setGame(game);
+		
+		entity = repository.save(entity);
+		return new RecordDTO(entity);
+		
+	}
+
+}
